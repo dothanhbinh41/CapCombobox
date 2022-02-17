0219f0ed8efbaabc36226ced30c0a28b6d1c1182
Đỗ Thanh Bình <binhdothanh.navn@gmail.com>
2022-02-17 14:02:13 +0700

    Add project files.

---
+++ CapCombobox/CapCombobox/CapComboboxView.cs
@@ -0,0 +1,28 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CapCombobox
+{
+    public class CapComboboxView : ContentView
+    {
+        public CapComboboxView()
+        {
+            BackgroundColor = Color.Yellow;
+            Content = new Button { Text = "Click me Now" };
+            if (Content is Button btn)
+            {
+                btn.Clicked += Btn_Clicked;
+            }
+        }
+
+        private void Btn_Clicked(object sender, EventArgs e)
+        {
+            var popup = new ComboboxPopup(X, Y, Width, Height);
+            var width = Xamarin.Essentials.DeviceDisplay.MainDisplayInfo.Width / Xamarin.Essentials.DeviceDisplay.MainDisplayInfo.Density;
+            PopupNavigation.Instance.PushAsync(popup, false);
+        }
+    }
+}
---
+++ CapCombobox/CapCombobox/ComboboxPopup.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Rg.Plugins.Popup.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CapCombobox
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ComboboxPopup : PopupPage
+    {
+        public ComboboxPopup()
+        {
+            InitializeComponent();
+            content.SizeChanged += Content_SizeChanged;
+        }
+
+        private void Content_SizeChanged(object sender, EventArgs e)
+        {
+            var size = content.Width;
+        }
+
+        double x, y, w, h;
+        public ComboboxPopup(double x, double y, double w, double h) : this()
+        {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            content.TranslateTo(x, y,length:0);
+            content.WidthRequest = w;
+            ForceLayout();
+        }
+    }
+}
